a946837283ffd10fd453f5ae47bb4c62ae82a34b
samvel63 <mhitaryan.1998@mail.ru>
2017-04-04 01:09:08 +0300

    Добавлена функция добавления вершины в двоичное дерево.

--- been_tree.c
+++ been_tree.c
@@ -7,6 +7,29 @@ struct bin_tree {
 };
 typedef struct bin_tree node;
 
+void insert(node ** tree, int val)
+{
+    node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = (node *)malloc(sizeof(node));
+        temp->left = temp->right = NULL;
+        temp->data = val;
+        *tree = temp;
+        return;
+    }
+
+    if(val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if(val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+
+}
+
 int main(void)
 {
     int *list_parent_vertex;
