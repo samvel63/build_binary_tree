e024a8311b54ec9f81d37b0a141cb3eb48f7f9b2
samvel63 <mhitaryan.1998@mail.ru>
2017-04-04 01:06:17 +0300

    Добавлена структура данных - двоичное дерево поиска

---
+++ been_tree.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct bin_tree {
+    int data;
+    struct bin_tree * right, * left;
+};
+typedef struct bin_tree node;
+
+int main(void)
+{
+    int *list_parent_vertex;
+    node *root;
+
+    return 0;
+}
