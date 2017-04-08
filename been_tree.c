16d78a1956d17c942e51d98632a9218804ea5e4f
samvel63 <mhitaryan.1998@mail.ru>
2017-04-08 12:17:59 +0300

    Изменен алгоритм программы.

--- been_tree.c
+++ been_tree.c
@@ -32,90 +32,40 @@ void insert(node ** tree, int val)
 
 }
 
-void find_parent_vertex(node **tree, int val, int *list_parent_vertices, int *a)
+int find_max_deep_nodes(node *tree, int deep)
 {
-    if (((*tree)->right && (*tree)->right->data == val) || ((*tree)->left && (*tree)->left->data == val)) {
-            list_parent_vertices = (int*)realloc(list_parent_vertices, (*a + 1) * sizeof(int));
-            list_parent_vertices[*a] = (*tree)->data;
-            ++*a;
-        
-    } else if(val > (*tree)->data) {
-        find_parent_vertex(&((*tree)->right), val, &list_parent_vertices[0], a);
-    } else if (val < (*tree)->data) {
-        find_parent_vertex(&((*tree)->left), val, &list_parent_vertices[0], a);
-    } 
+    if (!tree)
+        return deep - 1;
+
+    return max(find_max_deep_nodes(tree->left, deep + 1), find_max_deep_nodes(tree->right, deep + 1));
 }
 
-void find_terminal_vertex(node **tree, node **root, int *list_parent_vertices, int *a) {
-    if (!((*tree)->right) && !((*tree)->left)) {
-        find_parent_vertex(&(*root), (*tree)->data, &list_parent_vertices[0], a);
-    }
-
-    if ((*tree)->right)
-        find_terminal_vertex(&((*tree)->right), &(*root), &list_parent_vertices[0], a);
-    if ((*tree)->left)
-        find_terminal_vertex(&((*tree)->left), &(*root), &list_parent_vertices[0], a);
-}
-
-int find_deep_vertex(node **tree, int ver, int deep)
+void print_not_terminal_vertices(node *tree, int cur_deep, int deep)
 {
-    if ((*tree)->data == ver)
-        return deep;
-    if ((*tree)->data > ver)
-        return find_deep_vertex(&((*tree)->left), ver, deep + 1);
-    if ((*tree)->data < ver)
-        return find_deep_vertex(&((*tree)->right), ver, deep + 1);
-}
+    if (!tree)
+        return 0;
 
-int count_same_numbers(int num, int *arr) {
-    int count = 0;
-    for (int i = 0; i < sizeof(arr) / sizeof(int); ++i) {
-        if (num == arr[i])
-            ++count;
+    if (cur_deep == deep) {
+        printf("Вершина со значением %d является нетерминальной с максимальной глубиной.\n", tree->data);
+    } else {
+        print_not_terminal_vertices(tree->left, cur_deep + 1, deep);
+        print_not_terminal_vertices(tree->right, cur_deep + 1, deep);
     }
-    return count;
-}
-
-void print_max_deep_vertex(int deep, int *list1, int *list2)
-{
-    printf("Максимально глубокая(ие) нетерминальная(ые) вершина(ы) со значением(ями) "); 
-    int count = count_same_numbers(deep, &list1[0]);
-    for (int i = 0; i < count; ++i) {
-        while (deep != *list1) {
-            *list1++;
-            *list2++;       
-        }
-        printf("%d ", *list2);
-        *list1++;
-        *list2++;
-    }
-    printf("\n");
 }
 
 int main(void)
 {
-    int cap_not_terminal_vertex = 0, max_deep = 0;
-    int *list_parent_vertices;
+    int max_deep = 0;
+
     node *root;
 
-    list_parent_vertices = (int *) malloc (sizeof(int));
-    
     root = NULL;
     /* Inserting nodes into tree */
     int ver = 0;
     while (scanf("%d", &ver) == 1)
         insert(&root, ver);
-
-    find_terminal_vertex(&root, &root, &list_parent_vertices[0], &cap_not_terminal_vertex);
-    int list_deep_vertices[cap_not_terminal_vertex];
-
-    for (int i = 0; i < cap_not_terminal_vertex; ++i) {
-        list_deep_vertices[i] = find_deep_vertex(&root, list_parent_vertices[i], 0);
-        max_deep = max(max_deep, list_deep_vertices[i]);
-       // printf("%d %d\n", list_parent_vertices[i], list_deep_vertices[i]);
-    }
-    print_max_deep_vertex(max_deep, &list_deep_vertices[0], &list_parent_vertices[0]);
-    free(list_parent_vertices);
-
+    max_deep = find_max_deep_nodes(root, 1);
+    print_not_terminal_vertices(root, 1, max_deep);
+    printf("%d\n", max_deep);
     return 0;
 }
